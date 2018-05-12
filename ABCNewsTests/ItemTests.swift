3275bc56fb0a85c54c0085a270eeba5c59c3df01
nafisar <ios.nafisa@gmail.com>
2018-05-12 22:51:57 +1000

    unit test for item model

--- ABCNewsTests/ItemTests.swift
+++ ABCNewsTests/ItemTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import ABCNews
 
 class ItemTests: XCTestCase {
     
@@ -20,9 +21,36 @@ class ItemTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testItem() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let string = """
+            {
+                     "title":"No time to lounge around as hundreds turn out for couch surfing race",
+                     "pubDate":"2018-04-18 08:37:22",
+                     "link":"http:\\/\\/www.abc.net.au\\/news\\/2018-04-18\\/hundreds-descend-on-darwin-for-seventh-annual-couch-surfing-race\\/9672606",
+                     "guid":"http:\\/\\/www.abc.net.au\\/news\\/2018-04-18\\/hundreds-descend-on-darwin-for-seventh-annual-couch-surfing-race\\/9672606",
+                     "author":"Phoebe Hosier",
+                     "thumbnail":"http:\\/\\/www.abc.net.au\\/news\\/image\\/9673072-4x3-140x105.jpg",
+                     "description":"<p>More than a dozen teams from across the NT converge on Darwin to show off their skills, and sofas, in a bid to highlight youth homelessness.<\\/p>",
+                     "content":"<p>More than a dozen teams from across the NT converge on Darwin to show off their skills, and sofas, in a bid to highlight youth homelessness.<\\/p>",
+                     "enclosure":{
+                              "link":"http:\\/\\/www.abc.net.au\\/news\\/image\\/9673072-16x9-2150x1210.jpg",
+                              "type":"image\\/jpeg",
+                              "thumbnail":"http:\\/\\/www.abc.net.au\\/news\\/image\\/9673072-4x3-140x105.jpg"
+                }
+            }
+            """
+        let data = string.data(using: .utf8)
+        let result = try JSONDecoder().decode(Item.self, from:data! )
+        
+        XCTAssertEqual(result.title,"No time to lounge around as hundreds turn out for couch surfing race")
+        
+        XCTAssertEqual(result.pubDate, "2018-04-18 08:37:22")
+        
+        XCTAssertEqual(result.link, "http://www.abc.net.au/news/image/9673072-16x9-2150x1210.jpg")
+        
+        XCTAssertEqual(result.thumbnail, "http://www.abc.net.au/news/image/9673072-4x3-140x105.jpg")
     }
     
     func testPerformanceExample() {
